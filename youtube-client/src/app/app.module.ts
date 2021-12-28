d7cb9289413c782376b785522e247f4b45596781
AnatoliRub <catok1992@gmail.com>
2021-12-28 15:28:03 +0300

    feat: add logo and youtube components

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { YoutubeComponent } from './components/svgs/youtube/youtube.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, LogoComponent, YoutubeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
