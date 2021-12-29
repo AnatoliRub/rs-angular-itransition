cde2585c27a908a5b5ffaa769a741672ce257ef1
AnatoliRub <catok1992@gmail.com>
2021-12-29 22:29:28 +0300

    fix: remove empty providers

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -20,7 +20,6 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
