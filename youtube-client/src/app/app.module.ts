283d21dc3e804bb49153ad33e63bcdcaab4f7aef
AnatoliRub <catok1992@gmail.com>
2021-12-30 13:25:58 +0300

    feat: add view, like, dislike, comment svg components

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { LogoComponent } from './components/logo/logo.component';
 import { YoutubeComponent } from './components/svgs/youtube/youtube.component';
 import { SearchComponent } from './components/search/search.component';
 import { LoginInfoComponent } from './components/login-info/login-info.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
+import { CardComponent } from './components/card/card.component';
+import { ViewComponent } from './components/svgs/view/view.component';
+import { LikeComponent } from './components/svgs/like/like.component';
+import { DislikeComponent } from './components/svgs/dislike/dislike.component';
+import { CommentComponent } from './components/svgs/comment/comment.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +24,12 @@ import { LoginInfoComponent } from './components/login-info/login-info.component
     YoutubeComponent,
     SearchComponent,
     LoginInfoComponent,
+    SearchResultComponent,
+    CardComponent,
+    ViewComponent,
+    LikeComponent,
+    DislikeComponent,
+    CommentComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   bootstrap: [AppComponent],
