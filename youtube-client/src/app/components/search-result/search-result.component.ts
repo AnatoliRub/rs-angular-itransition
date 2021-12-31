a53112ddb9f306939e7e4d5c5d947ce2e4696f45
AnatoliRub <catok1992@gmail.com>
2021-12-31 13:58:15 +0300

    feat: add search-results component

---
+++ youtube-client/src/app/components/search-result/search-result.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Post, YoutubeData } from 'src/types/youtube-data';
+import * as Data from './data.json';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.scss'],
+})
+export class SearchResultComponent {
+  json: YoutubeData = Data;
+
+  posts: Post[] = [];
+
+  constructor() {
+    this.posts = this.json.items;
+    console.log(this.json.items);
+  }
+}
