586581c65096347ed274686ef7c422cb2b9927ef
AnatoliRub <catok1992@gmail.com>
2022-01-02 22:36:13 +0300

    feat: change project structure and moved settings button into a separate component

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -6,17 +6,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { LogoComponent } from './components/logo/logo.component';
+import { LogoComponent } from './components/header/logo/logo.component';
 import { YoutubeComponent } from './components/svgs/youtube/youtube.component';
-import { SearchComponent } from './components/search/search.component';
-import { LoginInfoComponent } from './components/login-info/login-info.component';
+import { SearchComponent } from './components/header/search/search.component';
+import { LoginInfoComponent } from './components/header/login-info/login-info.component';
 import { SearchResultComponent } from './components/search-result/search-result.component';
-import { CardComponent } from './components/card/card.component';
+import { CardComponent } from './components/search-result/card/card.component';
 import { ViewComponent } from './components/svgs/view/view.component';
 import { LikeComponent } from './components/svgs/like/like.component';
 import { DislikeComponent } from './components/svgs/dislike/dislike.component';
 import { CommentComponent } from './components/svgs/comment/comment.component';
 import { MaterialModule } from './material/material.module';
+import { SearchSettingsComponent } from './components/header/search-settings/search-settings.component';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { MaterialModule } from './material/material.module';
     LikeComponent,
     DislikeComponent,
     CommentComponent,
+    SearchSettingsComponent,
   ],
   imports: [
     BrowserModule,
