b4a4e009545d851fb0f7d42c69b1d49e2649bf33
AnatoliRub <catok1992@gmail.com>
2022-01-12 12:21:22 +0300

    fix: delete fake post

--- youtube-client/src/app/components/search-result/card/card.component.ts
+++ youtube-client/src/app/components/search-result/card/card.component.ts
@@ -8,54 +8,15 @@ import { Post } from 'src/types/youtube-data';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
-  @Input() post: Post = {
-    kind: '',
-    etag: '',
-    id: '',
-    snippet: {
-      channelTitle: '',
-      description: '',
-      publishedAt: '',
-      title: '',
-      thumbnails: {
-        default: {
-          url: '',
-          width: 0,
-          height: 0,
-        },
-        high: {
-          url: '',
-          width: 0,
-          height: 0,
-        },
-        maxres: {
-          url: '',
-          width: 0,
-          height: 0,
-        },
-        medium: {
-          url: '',
-          width: 0,
-          height: 0,
-        },
-        standard: {
-          url: '',
-          width: 0,
-          height: 0,
-        },
-      },
-    },
-    statistics: {
-      commentCount: '',
-      dislikeCount: '',
-      likeCount: '',
-      viewCount: '',
-    },
-  };
+  @Input() post: Post | undefined;
+
+  constructor() {
+    this.post = undefined;
+  }
 
   setCardBorderColor() {
     const dateNow = new Date();
-    const datePublish = new Date(this.post.snippet.publishedAt);
+    const datePublish = new Date(this.post!.snippet.publishedAt);
     const difference = Math.floor((dateNow.getTime() - datePublish.getTime()) / 86400000);
 
     if (difference < 7) {
