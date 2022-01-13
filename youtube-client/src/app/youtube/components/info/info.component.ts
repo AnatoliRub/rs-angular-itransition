fe73e9d3c174239faa957f8be95da2d51609b55c
AnatoliRub <catok1992@gmail.com>
2022-01-13 13:20:32 +0300

    add info component and use it into the card component

---
+++ youtube-client/src/app/youtube/components/info/info.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Statistic } from 'src/types/youtube-data';
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.scss'],
+})
+export class InfoComponent {
+  @Input() statistics: Statistic | undefined = undefined;
+}
--- youtube-client/src/app/youtube/youtube.module.ts
+++ youtube-client/src/app/youtube/youtube.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SearchResultComponent } from './components/search-result/search-result.component';
 import { CommentComponent } from '../components/svgs/comment/comment.component';
 import { DislikeComponent } from '../components/svgs/dislike/dislike.component';
@@ -9,6 +10,9 @@ import { CardComponent } from './components/search-result/card/card.component';
 import { MaterialModule } from '../material/material.module';
 import { FilterDataByCriteriaPipe } from './pipes/filter-data-by-criteria.pipe';
 import { SearchByWordPipe } from './pipes/search-by-word.pipe';
+import { DetailedPageComponent } from './components/detailed-page/detailed-page.component';
+import { InfoItemComponent } from './components/info-item/info-item.component';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
@@ -19,9 +23,12 @@ import { SearchByWordPipe } from './pipes/search-by-word.pipe';
     DislikeComponent,
     CommentComponent,
     SearchByWordPipe,
-    FilterDataByCriteriaPipe
+    FilterDataByCriteriaPipe,
+    DetailedPageComponent,
+    InfoItemComponent,
+    InfoComponent,
   ],
-  exports: [SearchResultComponent],
-  imports: [CommonModule, MaterialModule],
+  exports: [SearchResultComponent, DetailedPageComponent],
+  imports: [CommonModule, MaterialModule, FontAwesomeModule],
 })
 export class YoutubeModule {}
