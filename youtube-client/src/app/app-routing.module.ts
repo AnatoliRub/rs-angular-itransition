a9a93d5d0cc02e60761ff806fc87528b4475a117
AnatoliRub <catok1992@gmail.com>
2022-01-17 15:14:01 +0300

    feat: add app-routing module

--- youtube-client/src/app/app-routing.module.ts
+++ youtube-client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
+import { DetailedPageComponent } from './youtube/pages/detailed-page/detailed-page.component';
+import { SearchResultPageComponent } from './youtube/pages/search-result-page/search-result-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchResultPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'info', component: DetailedPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
