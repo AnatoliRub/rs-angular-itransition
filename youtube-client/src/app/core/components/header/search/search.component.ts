ffe85c6d85b9a2968e4279c73cac4f63fa5b4f96
Anatoly <catok1992@gmail.com>
2022-01-28 00:27:43 +0300

    feat: implement debounce functionality and remove search button

--- youtube-client/src/app/core/components/header/search/search.component.ts
+++ youtube-client/src/app/core/components/header/search/search.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { debounceTime, filter, fromEvent, Subscription, tap } from 'rxjs';
 import { SearchServiceService } from 'src/app/core/services/search-service.service';
 import { RoutesPath } from 'src/app/routes.enum';
 
@@ -8,7 +9,12 @@ import { RoutesPath } from 'src/app/routes.enum';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('search')
+  search?: ElementRef;
+
+  subscription$?: Subscription;
+
   searchString: string = '';
 
   constructor(
@@ -16,8 +22,22 @@ export class SearchComponent {
     private readonly router: Router,
   ) {}
 
-  displayResults() {
-    this.router.navigate([RoutesPath.Youtube]);
-    this.searchService.setSearchWord(this.searchString);
+  ngAfterViewInit(): void {
+    if (this.search) {
+      this.subscription$ = fromEvent<Event>(this.search.nativeElement, 'change')
+        .pipe(
+          filter((val) => (val?.target as HTMLInputElement).value.length > 3),
+          debounceTime(400),
+          tap((val) => {
+            this.router.navigate([RoutesPath.Youtube]);
+            this.searchService.setSearchWord((val?.target as HTMLInputElement).value);
+          }),
+        )
+        .subscribe();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$?.unsubscribe();
   }
 }
