7b0435de67513743c9414b5e66a2c175b918468f
Anatoly <catok1992@gmail.com>
2022-02-03 11:18:30 +0300

    feat: move svg components to youtube module components

--- youtube-client/src/app/youtube/youtube.module.ts
+++ youtube-client/src/app/youtube/youtube.module.ts
@@ -3,10 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { SearchResultPageComponent } from './pages/search-result-page/search-result-page.component';
-import { CommentComponent } from '../components/svgs/comment/comment.component';
-import { DislikeComponent } from '../components/svgs/dislike/dislike.component';
-import { LikeComponent } from '../components/svgs/like/like.component';
-import { ViewComponent } from '../components/svgs/view/view.component';
 import { CardComponent } from './components/card/card.component';
 import { MaterialModule } from '../material/material.module';
 import { FilterDataByCriteriaPipe } from './pipes/filter-data-by-criteria.pipe';
@@ -15,6 +11,10 @@ import { InfoItemComponent } from './components/info-item/info-item.component';
 import { InfoComponent } from './components/info/info.component';
 import { RoutesPath } from '../routes.enum';
 import { AuthGuard } from '../auth/guards/auth.guard';
+import { CommentComponent } from './components/svgs/comment/comment.component';
+import { DislikeComponent } from './components/svgs/dislike/dislike.component';
+import { LikeComponent } from './components/svgs/like/like.component';
+import { ViewComponent } from './components/svgs/view/view.component';
 
 @NgModule({
   declarations: [
