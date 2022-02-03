ea92423869b47bdcc340ae6628548a1579b1c992
Anatoly <catok1992@gmail.com>
2022-02-03 12:29:34 +0300

    feat: add lazy-loading for technical module

--- youtube-client/src/app/technical/technical.module.ts
+++ youtube-client/src/app/technical/technical.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ErrorComponent } from '../auth/components/error/error.component';
+import { RouterModule } from '@angular/router';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { ErrorComponent } from './components/svgs/error/error.component';
+import { RoutesPath } from '../routes.enum';
 
 @NgModule({
   declarations: [ErrorPageComponent, ErrorComponent],
-  imports: [CommonModule],
+  exports: [ErrorPageComponent, ErrorComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([{ path: RoutesPath.Main, component: ErrorPageComponent }]),
+  ],
 })
 export class TechnicalModule {}
