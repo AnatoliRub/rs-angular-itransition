1899736e400ae2ff4760199602d75c432a626ee0
Anatoly <catok1992@gmail.com>
2022-02-06 17:56:08 +0300

    feat: add button which lead to admin page to search-result-page component

--- youtube-client/src/app/youtube/pages/search-result-page/search-result-page.component.ts
+++ youtube-client/src/app/youtube/pages/search-result-page/search-result-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SearchServiceService } from 'src/app/core/services/search-service.service';
+import { RoutesPath } from 'src/app/routes.enum';
 import { IFilterData } from 'src/types/filtering-criteria-types';
 import { Post } from 'src/types/youtube-data';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { CardService } from '../../services/card.service';
 
 @Component({
@@ -11,12 +14,21 @@ import { CardService } from '../../services/card.service';
   styleUrls: ['./search-result-page.component.scss'],
 })
 export class SearchResultPageComponent {
+  faPlus = faPlus;
+
   posts$: Observable<Post<string>[]> = this.cardServise.posts;
+
+  pathToAdminPage: RoutesPath[] = [RoutesPath.Admin];
 
   filter$: Observable<IFilterData> = this.searchService.filter;
 
   constructor(
     private readonly searchService: SearchServiceService,
     private readonly cardServise: CardService,
+    private readonly router: Router,
   ) {}
+
+  goToAdminPage() {
+    this.router.navigateByUrl(RoutesPath.Admin);
+  }
 }
