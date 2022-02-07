7bdef53399c8330813ab5dfd1661ee92ecc03688
Anatoly <catok1992@gmail.com>
2022-02-07 16:18:16 +0300

    feat: add support to show admin error

--- youtube-client/src/app/technical/pages/error-page/error-page.component.ts
+++ youtube-client/src/app/technical/pages/error-page/error-page.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
   styleUrls: ['./error-page.component.scss'],
 })
-export class ErrorPageComponent {}
+export class ErrorPageComponent implements OnInit, OnDestroy {
+  subscription?: Subscription;
+
+  adminError?: boolean;
+
+  constructor(private readonly route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // eslint-disable-next-line no-console
+    this.subscription = this.route.queryParams.subscribe((val) => {
+      this.adminError = val['admin'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+}
