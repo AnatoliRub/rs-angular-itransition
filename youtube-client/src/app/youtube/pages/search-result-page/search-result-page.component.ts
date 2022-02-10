e1754c054638864cee6fddb2d2f880daea7bafd2
Anatoly <catok1992@gmail.com>
2022-02-11 02:48:33 +0300

    feat: use selector in search result component

--- youtube-client/src/app/youtube/pages/search-result-page/search-result-page.component.ts
+++ youtube-client/src/app/youtube/pages/search-result-page/search-result-page.component.ts
@@ -3,14 +3,13 @@ import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { RoutesPath } from 'src/app/routes.enum';
 import { IFilterData } from 'src/types/filtering-criteria-types';
-import { Post } from 'src/types/youtube-data';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { Store } from '@ngrx/store';
 import { CustomCard } from 'src/app/admin/models/card.model';
 import { selectGetAllCustomCardsSelector } from 'src/app/admin/ngrx/selectors/custom-card.selector';
 import { selectSearchFilter } from 'src/app/core/ngrx/selectors/search.selectors';
-import { CardService } from '../../services/card.service';
 import { selectIsAdminRole } from '../../../auth/ngrx/selectors/auth.selectors';
+import { selectPostsSelector } from '../../ngrx/selectors/posts.selectors';
 
 @Component({
   selector: 'app-search-result',
@@ -20,7 +19,7 @@ import { selectIsAdminRole } from '../../../auth/ngrx/selectors/auth.selectors';
 export class SearchResultPageComponent {
   faPlus = faPlus;
 
-  posts$: Observable<Post<string>[]> = this.cardServise.posts;
+  posts$ = this.store.select(selectPostsSelector);
 
   customCards$: Observable<CustomCard[]> = this.store.select(selectGetAllCustomCardsSelector);
 
@@ -30,11 +29,7 @@ export class SearchResultPageComponent {
 
   isAdmin$: Observable<boolean> = this.store.select(selectIsAdminRole);
 
-  constructor(
-    private readonly cardServise: CardService,
-    private readonly router: Router,
-    private readonly store: Store,
-  ) {}
+  constructor(private readonly router: Router, private readonly store: Store) {}
 
   goToAdminPage() {
     this.router.navigateByUrl(RoutesPath.Admin);
