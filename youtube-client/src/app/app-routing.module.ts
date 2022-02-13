cb18f4c97e39da10a0f520100a25600ff5582e67
Anatoly <catok1992@gmail.com>
2022-02-13 20:12:49 +0300

    fix: fix return types for loadChildren callbacks

--- youtube-client/src/app/app-routing.module.ts
+++ youtube-client/src/app/app-routing.module.ts
@@ -1,25 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
 import { RoutesPath } from './routes.enum';
+import { TechnicalModule } from './technical/technical.module';
+import { YoutubeModule } from './youtube/youtube.module';
 
 const routes: Routes = [
   {
     path: RoutesPath.Youtube,
-    loadChildren: (): Promise<any> =>
+    loadChildren: (): Promise<typeof YoutubeModule> =>
       import('./youtube/youtube.module').then((m) => m.YoutubeModule),
   },
   {
     path: RoutesPath.Auth,
-    loadChildren: (): Promise<any> => import('./auth/auth.module').then((m) => m.AuthModule),
+    loadChildren: (): Promise<typeof AuthModule> =>
+      import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: RoutesPath.Admin,
-    loadChildren: (): Promise<any> => import('./admin/admin.module').then((m) => m.AdminModule),
+    loadChildren: (): Promise<typeof AdminModule> =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
   },
   { path: RoutesPath.Main, redirectTo: RoutesPath.Youtube, pathMatch: 'full' },
   {
     path: RoutesPath.Error,
-    loadChildren: (): Promise<any> =>
+    loadChildren: (): Promise<typeof TechnicalModule> =>
       import('./technical/technical.module').then((m) => m.TechnicalModule),
   },
   { path: RoutesPath.Redirect, redirectTo: RoutesPath.Error },
