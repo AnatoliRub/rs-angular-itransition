336dbf9747067d40c8e5b049e5145d498726d837
Anatoly <catok1992@gmail.com>
2022-02-14 19:02:14 +0300

    fix: remove redundant logic

--- youtube-client/src/app/admin/models/user.model.ts
+++ youtube-client/src/app/admin/models/user.model.ts
@@ -10,22 +10,9 @@ export class User {
   }
 
   isAdminRole(): boolean {
-    const userData = this.getSortEntries(this.user);
-
-    const adminData = this.getSortEntries(this.admin);
-
     return (
-      userData.length === adminData.length &&
-      userData.every(([userKey, userValue]: [string, string], i) => {
-        const [adminKey, adminValue] = adminData[i];
-        return userKey === adminKey && userValue === adminValue;
-      })
-    );
-  }
-
-  getSortEntries(user: UserData): [string, string][] {
-    return Object.entries(user).sort((a: [string, string], b: [string, string]) =>
-      a[0].localeCompare(b[0]),
+      this.admin.loginData === this.user.loginData &&
+      this.admin.passwordData === this.user.passwordData
     );
   }
 }
